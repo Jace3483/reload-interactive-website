8c75d27a5fd1d227e6d71d1d7479837cbbc5d723
Jace <jacebradford04@gmail.com>
2025-11-14 08:22:44 -0500

    new

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import { BrowserRouter as Router, Routes, Route, Link, useLocation, useNavigate } from 'react-router-dom';
-import { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Home';
 import Landing from './Landing';
-
-// RemoveTrailingSlash component
-function RemoveTrailingSlash() {
-  const location = useLocation();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (location.pathname !== "/" && location.pathname.endsWith("/")) {
-      navigate(location.pathname.slice(0, -1) + location.search, { replace: true });
-    }
-  }, [location, navigate]);
-
-  return null;
-}
 
 function App() {
   return (
     <Router>
-      <RemoveTrailingSlash /> {/* Include it here */}
       <div>
         {/* Navbar */}
         <nav style={{
