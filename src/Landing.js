22aead85f939f78d7157113617829a070bcc4a5b
Jace <jacebradford04@gmail.com>
2025-11-14 09:00:11 -0500

    Landing page update

--- src/Landing.js
+++ src/Landing.js
@@ -12,7 +12,7 @@ function Landing() {
       {/* Navbar */}
       <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
         <Container>
-          <Navbar.Brand href="#" style={{fontWeight: 900}}>Reload Interactive</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/" style={{fontWeight: 900}}>Reload Interactive</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
