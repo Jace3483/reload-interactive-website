22ac318ef2c37f460cccc8ada4ab1e29bd1d001e
Jace <jacebradford04@gmail.com>
2025-11-18 10:19:43 -0500

    tester

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 // App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Signup from './Signup';
 import MyAccount from './MyAccount';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/signup" element={<Signup />} />
         <Route path="/myaccount" element={<MyAccount />} />
         <Route path="*" element={<p>Page not found</p>} />
       </Routes>
