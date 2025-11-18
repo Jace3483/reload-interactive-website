3c12b0634112ac098805b9a122faaf7e6ca41c94
Jace <jacebradford04@gmail.com>
2025-11-18 10:22:42 -0500

    new

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 // App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MyAccount from './MyAccount';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/myaccount" element={<MyAccount />} />
         <Route path="*" element={<p>Page not found</p>} />
       </Routes>
     </Router>
